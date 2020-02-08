eedc3366bae7944072c737045828842666c941cc
user164622 <gabriel.sgaspar@yahoo.com.br>
2020-02-08 16:44:59 -0500

    criando componentes da tela de login programaticamente

--- noStoryBoard/ViewController.swift
+++ noStoryBoard/ViewController.swift
@@ -9,12 +9,54 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    // Criando o container onde vai entrar os elementos do formulario de login
+    private var loginContentView: UIView = {
+        let view = UIView()
+        view.backgroundColor = UIColor.green
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    private var loginTextField: UITextField = {
+        let textField = UITextField()
+        textField.backgroundColor = UIColor.white
+        textField.borderStyle = .roundedRect
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        return textField
+    }()
+    
+    private var passwordTextField: UITextField = {
+        let passwordField = UITextField()
+        passwordField.backgroundColor = UIColor.white
+        passwordField.borderStyle = .roundedRect
+        passwordField.translatesAutoresizingMaskIntoConstraints = false
+        return passwordField
+    }()
+    
+    private var loginButton: UIButton = {
+        let loginBtn = UIButton(type: .system)
+        loginBtn.backgroundColor = UIColor.blue
+        loginBtn.setTitle("Login", for: .normal)
+        loginBtn.tintColor = UIColor.white
+        loginBtn.layer.cornerRadius = 5
+        loginBtn.clipsToBounds = true
+        loginBtn.translatesAutoresizingMaskIntoConstraints = false
+        return loginBtn
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
         view.backgroundColor = UIColor.orange
+        
+        loginContentView.addSubview(loginTextField)
+        loginContentView.addSubview(passwordTextField)
+        loginContentView.addSubview(loginButton)
+        
+        view.addSubview(loginContentView)
+        
     }
 
 
